ddfb55b4d02fbc10b5e1daf7186ffd773d6d4c74
Tpessia <30393840+Tpessia@users.noreply.github.com>
2017-11-19 23:42:27 -0200

    Add files via upload

--- SubmarinoYellow/src/submarinoyellow/SubmarinoYellow.java
+++ SubmarinoYellow/src/submarinoyellow/SubmarinoYellow.java
@@ -9,104 +9,106 @@ public class SubmarinoYellow {
 
     public static void main(String[] args) {
         Scanner entrada = new Scanner(System.in);
-        Scanner entradaS = new Scanner(System.in);
+        Scanner entradaS = new Scanner(System.in); //Scanner para Strings
 
         int menu, prodNum = 0, vendasNum = 0;
+        //menu é utilizado para nevegação no menu; prodNum é o contador de produtos cadastrados; vendasNum é o contador de compras efetuadas;
         boolean exit = false;
+        //exit é utilizado para sair do programa (quando exit == true)
 
-        int[] codigos = {1,2,3,4,5,6,7,8,9,10};
-        String[] nomes = new String[10];
-        Double[] valores = new Double[10];
+        int[] codigos = {1,2,3,4,5,6,7,8,9,10}; //codigos para os produtos cadastrados
+        String[] nomes = new String[10]; //nomes dos produtos cadastrados
+        Double[] valores = new Double[10]; //valor dos produtos cadastrados
 
-        int[] vendas = new int[100];
+        int[] vendas = new int[100]; //código de cada produto vendido
 
-        clean();
+        clean(); //função que "limpa" o console
 
-        print("Projeto \"Loja de Produtos\" - Professora Simone de Abreu | Contrução de Algoritmos\n");
+        print("Projeto \"Loja de Produtos\" - Professora Simone de Abreu | Contrução de Algoritmos\n"); //print é um atalho para System.out.println(), que também altera os caracteres para UTF-8
         print("Nome: Douglas RA:");
         print("Nome: Nathalia RA:");
         print("Nome: Thiago RA:");
         print("Nome: Thiago Vinícius Pessia RA: 21027210");
         print("Nome: Victor RA:");
 
-        print("\nAperte enter para continuar:");
-        String sair = entradaS.nextLine();
+        continuar();
 
         do {
             clean();
 
-            print("========= Menu de Opções =========\n1 - Cadastrar produto\n2 - Listar produtos\n3 - Vender produto\n4 - Relatório de vendas\n5 - Sair\nDigite uma opção:");
+            print("========= Menu de Opções =========");
+            print("1 - Cadastrar produto");
+            print("2 - Listar produtos");
+            print("3 - Vender produto");
+            print("4 - Relatório de vendas");
+            print("5 - Sair");
+            print("Digite uma opção:");
             menu = entrada.nextInt();
 
             clean();
 
             switch(menu) {
                 case 1: //cadastro
-                    if(prodNum<10) {
-                        Double valorTemp;
-
+                    if(prodNum<10) { //verifica se o número máximo de produtos já foi cadastrado
                         print("Insira o nome do produto (" + (prodNum + 1) + "/10):");
                         nomes[prodNum] = entradaS.nextLine();
 
 
                         print("\nInsira o valor do produto (" + (prodNum + 1) + "/10):");
-                        valorTemp = entrada.nextDouble();
+                        valores[prodNum] = entrada.nextDouble();
 
-                        if(valorTemp > 0){
-                            valores[prodNum] = valorTemp;
-
+                        if(valores[prodNum] > 0){ //verifica se o valor inserido é válido
                             clean();
 
                             print("CADASTRO EFETUADO!");
-                            prodNum++;
+                            prodNum++; //só considera o cadastro caso o valor seja validado
 
-                            sleep(2000);
+                            continuar();
                         }
                         else {
                             clean();
 
                             print("Valor Inválido");
 
-                            sleep(2000);
+                            continuar();
                         }
                     }
                     else {
                         print("Número máximo de cadastros atingido!");
-                        sleep(2000);
+                        continuar();
                     }
 
                     break;
 
                 case 2: //listagem
-                    listar(prodNum,codigos,nomes,valores);
+                    listar(prodNum,codigos,nomes,valores); //método que lista os produtos cadastrados
 
-                    print("Aperte enter para sair:");
-                    sair = entradaS.nextLine();
+                    continuar();
 
                     break;
 
                 case 3: //venda
                     if(prodNum!=0) {
-                        listar(prodNum,codigos,nomes,valores);
+                        listar(prodNum,codigos,nomes,valores); //lista os produtos cadastrados
 
-                        int vendaTemp;
+                        int vendaTemp; //mantém a venda em uma variável temporária, até que ela seja validada, e então envia o código para o vetor
 
-                        if(vendasNum<100) {
+                        if(vendasNum<=100) { //verifica se o número máximo de vendas já foi alcançado
                             print("Insira o código do produto que deseja comprar:");
 
                             vendaTemp = entrada.nextInt();
 
                             clean();
 
-                            if(vendaTemp < (prodNum + 1)) {
+                            if(vendaTemp < (prodNum + 1)) { //verifica se o código inserido é válido (se o código é menor ou igual ao ultimo código cadastrado)
                                 vendas[vendasNum] = vendaTemp;
                                 print("Compra Cadastrada!");
                                 vendasNum++;
-                                sleep(2000);
+                                continuar();
                             }
                             else {
                                 print("Código Inexistente!");
-                                sleep(2000);
+                                continuar();
                             }
                         }
                         else {
@@ -115,7 +117,7 @@ public class SubmarinoYellow {
                     }
                     else {
                         print("Nenhum produto cadastrado!");
-                        sleep(2000);
+                        continuar();
                     }
 
                     break;
@@ -123,12 +125,13 @@ public class SubmarinoYellow {
                 case 4: //relatório
                     if(vendas[0]!=0) {
 
-                        print("=== Relatório de Vendas ===");
+                        print("=== Relatório de Vendas ===\n");
 
-                        for (int i = 0; i < prodNum; i++) {
-                            int comprado = 0;
-                            for(int j = 0; j<vendas.length; j++) {
-                                if(vendas[j] == (i + 1)) {
+                        for (int i = 0; i < prodNum; i++) {  //itera sobre os produtos cadastrados
+                            int comprado = 0; //variável que guarda o número de vezes que um produto específico foi comprado
+
+                            for(int j = 0; j<vendas.length; j++) { //itera sobre as vendas
+                                if(vendas[j] == (i + 1)) { //verifica se o código no vetor vendas é igual ao código do produto em questão
                                     comprado++;
                                 }
                             }
@@ -146,8 +149,7 @@ public class SubmarinoYellow {
                         print("Nenhuma venda cadastrada!");
                     }
 
-                    print("\nAperte enter para sair:");
-                    sair = entradaS.nextLine();
+                    continuar();
 
                     break;
 
@@ -158,19 +160,19 @@ public class SubmarinoYellow {
 
                 default:
                     print("Número inserido é invalido!");
-                    sleep(2000);
+                    continuar();
             }
 
         } while(exit != true);
     }
 
     public static void listar(int num, int[] codigo, String[] nome, Double[] valor) { //método para listagem dos itens cadastrados
-        if (num == 0) {
+        if (num == 0) { //verifica se algum produto já foi cadastrado
             print("Nenhum produto cadastrado!");
         }
         else {
             for (int i = 0; i < num; i++) {
-                print("Código: " + codigo[i] + "\nNome: " + nome[i] + "\nValor: " + valor[i] + "\n");
+                print("Código: " + codigo[i] + "\nNome: " + nome[i] + "\nValor: " + valor[i] + "\n"); //imprime os dados do produto
             }
         }
     }
@@ -193,12 +195,9 @@ public class SubmarinoYellow {
         }
     }
 
-    public static void sleep(int miliseconds) { //intorrompe o programa por x milisegundos
-        try {
-            Thread.sleep(miliseconds);
-        }
-        catch(InterruptedException e) {
-
-        }
+    public static void continuar() { //intorrompe o programa momentaneamente
+        Scanner entry = new Scanner(System.in); //Scanner para Strings
+        print("\nAperte enter para sair");
+        String sair = entry.nextLine();
     }
 }
